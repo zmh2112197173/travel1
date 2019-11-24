fa07277be800300eee699f7cf83ccb5a3fe3873b
zmh2112197173 <2112197173@qq.com>
2019-11-24 22:46:41 +0800

    基本框架

---
+++ src/pages/map/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="width: 100%; height: 100%;">
+    <map
+      id="map"
+      scale="14"
+      :markers="markers"
+      show-location
+      style="width: 100%; height: 100%;"
+    ></map>
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      markers: [{
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      }]
+      
+    }
+  },
+
+
+
+  methods: {
+  },
+
+  created () {
+    
+  }
+}
+</script>
+
+<style>
+body, html {
+  height:100%
+}
+</style>
