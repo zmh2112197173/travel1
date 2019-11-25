f0a245b355acfeaa100f664e71b7b4f66988cc9a
zmh2112197173 <2112197173@qq.com>
2019-11-25 10:23:56 +0800

    基本页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-    <i-grid>
-    <i-grid-item>
-        <i-grid-icon>
-            <image src="/static/images/user.png" />
-        </i-grid-icon>
-        <i-grid-label>Grid</i-grid-label>
-    </i-grid-item>
-  </i-grid>
+    <swiper
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+    >
+    <block v-for="img in imgUrls" :key="img">
+      <swiper-item>
+        <image :src="img" style="width:100%;"/>
+      </swiper-item>
+    </block>
+    </swiper>
+    <i-grid i-class="no-border">
+      <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
+          <i-grid-icon>
+              <image :src="grid.image" />
+          </i-grid-icon>
+          <i-grid-label>{{grid.title}}</i-grid-label>
+      </i-grid-item>
+    </i-grid>
+    <i-panel :title="title_name">
+    <view style="padding: 15px;">
+      <i-card v-for="item in top" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+        <view slot="content">{{item.remark}}</view>
+        <view slot="footer">{{item.address}}</view>
+      </i-card>
+    </view>
+  </i-panel>
   </div>
 </template>
 
@@ -15,23 +35,50 @@
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      title_name: "强烈推荐",
+      grids: [
+        {title:"酒店客栈",image:"/static/images/1.png"},
+        {title:"大理美食",image:"/static/images/2.png"},
+        {title:"休闲娱乐",image:"/static/images/3.png"},
+        {title:"旅游攻略",image:"/static/images/4.png"}
+      ],
+      top: [
+        {name:"大理喜洲油粑粑",address:"大理爱情海喜洲店",image:"/static/images/8.jpg",remark:"大理的风味小吃，有甜咸两种口味，外皮香酥，内在松软，物美价廉"},
+        {name:"舒适大床房",address:"大理古城东门",image:"/static/images/10.jpg",remark:"环境十分好！房子整体以白族框架为主，装修带有简约欧式味道"},
+        {name:"大理苍山",address:"大理白族自治州云岭山脉南端",image:"/static/images/5.jpg",remark:"山色苍翠，国家级风景名胜区，是大理风花雪四大名景之最"},
+        {name:"大理清真乳扇",address:"大理清真伊味轩",image:"/static/images/9.jpg",remark:"牛奶做成扇子卖，它是一种奶酪，美味可口"}
+      ],
+       imgUrls: [
+        "/static/images/5.jpg",
+        "/static/images/6.jpg",
+        "/static/images/7.jpg"
+      ],
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000
     }
   },
 
 
   methods: {
+    goType(type){
+      console.log(type)
+      let url = '../list/main?type=' + type.title
+      mpvue.navigateTo({ url })
+    }
   },
 
   created () {
-    // let app = getApp()
   }
 }
 </script>
 
 <style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+div >>> .split {
+  margin-bottom: 10pt;
+}
 </style>
---
+++ src/pages/list/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <i-card v-for="item in list" :key="item" i-class="split" :extra="item.name" :thumb="item.image">
+      <view slot="content">{{item.remark}}</view>
+      <view slot="footer">{{item.address}}</view>
+    </i-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list:[]
+  }
+},
+onLoad (option){
+    this.list = require('@/data/' + option.type + '.json')
+    console.log(this.list)
+  },
+methods: {
+},
+created () {
+  }
+}
+</script>
+
+<style scoped>
+div >>> .split {
+  margin-bottom: 10pt;
+}
+</style>
